ff0fad670842e2f857929d6575fbeaa953809a41
Aditya Yadav <aditya023.ay@gmail.com>
2022-05-02 21:30:10 +0530

    refactored

--- src/Scroll.js
+++ src/Scroll.js
@@ -3,8 +3,10 @@ import React from 'react'
 const Scroll = (props) =>{
     return (
         <div style={{overflowY:'scroll',
-                    border: '5px sold black',
-                    height: '600px'
+                    border: '5px solid black',
+                    height: '600px',
+                    marginBottom:'10px',
+                    marginTop: '10px'
                     }}>
             {
                 props.children
